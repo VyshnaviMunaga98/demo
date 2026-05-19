d96543d02ce265c840b5886a92a26e4eb73187c8
Sai Munaga Vyshnavi <1500199@conneqtcorp.com>
2026-05-19 14:04:01 +0530

    Implemented Login Route logic to register/login user

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -3,10 +3,16 @@ const express = require("express");
 const router = express.Router();
 
 const {
+  register,
   login
 } = require("../controllers/authController");
 
 
+// REGISTER
+router.post("/register", register);
+
+
+// LOGIN
 router.post("/login", login);
 
 
